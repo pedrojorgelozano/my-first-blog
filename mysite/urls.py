9e323cdf3ad513435c80bb6da970e3d436759609
Pedro Jorge Lozano <pedrojorge.lozano@gmail.com>
2018-11-20 14:44:12 +0100

    otro commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/$',auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+	url(r'^logout/$', auth_views.LogoutView.as_view, name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls')),
 ]
