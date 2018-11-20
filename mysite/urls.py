83725d8cb49a3698513f611e231192477018ca7b
Pedro Jorge Lozano <pedrojorge.lozano@gmail.com>
2018-11-20 15:11:31 +0100

    funcionando

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$',auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-	url(r'^logout/$', auth_views.LogoutView.as_view, name='logout', kwargs={'next_page': '/'}),
+    #url(r'^registration/login/$', views.login, name='login'),
+    #url(r'^registration/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
