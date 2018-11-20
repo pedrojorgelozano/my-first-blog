df7e7060e1032ebe80ea14549e3044ba502dc07c
Pedro Jorge Lozano <pedrojorge.lozano@gmail.com>
2018-11-20 15:24:15 +0100

    anadir google site verification

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 
 from django.contrib.auth import views
 
+from django.http import HttpResponse
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     #url(r'^registration/login/$', views.login, name='login'),
     #url(r'^registration/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
+    url(r'^googlea55e63331b4c4a8a\.html$', lambda r: HttpResponse("google-site-verification: googlea55e63331b4c4a8a.html", content_type="text/plain")),
 ]
